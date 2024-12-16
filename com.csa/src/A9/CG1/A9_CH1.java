73f81ca341d692393cf1afb861ef0c2a146315bf
nitrogeo-tully <froxgames@outlook.com>
2024-12-16 12:45:24 -0600

    @ school csa backup

--- com.csa/src/A9/CG1/A9_CH1.java
+++ com.csa/src/A9/CG1/A9_CH1.java
@@ -1,7 +1,7 @@
 package A9.CG1;
 
 public class A9_CH1 {
-    public boolean contains(String value, String searchFor) {
+    public static boolean contains(String value, String searchFor) {
         return value.toLowerCase().contains(searchFor.toLowerCase());
     }
 }
---
+++ com.csa/src/A9/CG2/A9_Ch4.java
@@ -0,0 +1,4 @@
+package A9.CG2;
+
+public class A9_Ch4 {
+}
