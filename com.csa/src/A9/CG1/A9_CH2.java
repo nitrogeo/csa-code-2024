8e1be59acef8c649e5f7b432de1db6880e92f9e9
nitrogeo <FroxGames@outlook.com>
2024-12-16 01:09:45 -0600

    monday morning backup

--- com.csa/src/A9/CG1/A9_CH2.java
+++ com.csa/src/A9/CG1/A9_CH2.java
@@ -2,13 +2,10 @@ package A9.CG1;
 
 public class A9_CH2 {
     public String contains(String text) {
-        char[] textArrgs = text.toString();night
-        for (int iter = 0; iter < St.length; iter++) { // s2
-            temp[iter] = numbers[iter];
+        String threeDiv = "";
+        for (int iter = 0; iter < text.length(); iter+=3) { // s2
+            threeDiv += text.indexOf(iter);
         }
-        temp[temp.length - 1] = value /*numbers[iter]*/;
-        numbers = temp;
-        boolean container = value.toLowerCase().contains(searchFor.toLowerCase());
-        return container;
+        return threeDiv;
     }
 }
