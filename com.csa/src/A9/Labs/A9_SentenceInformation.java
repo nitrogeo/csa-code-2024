9b33987f38e68a4363a09676d3241ff9f869db8b
nitrogeo bauguss <froxgames@outlook.com>
2025-01-09 15:49:10 -0600

    @ school csa backup bauguss

--- com.csa/src/A9/Labs/A9_SentenceInformation.java
+++ com.csa/src/A9/Labs/A9_SentenceInformation.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class A9_SentenceInformation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter your sentence:");
+        System.out.print("Enter your sentence: \n\n");
         String sent = scan.nextLine();
+
+        System.out.println(wordCount(sent) + " words");
+        System.out.println((consonantCount(sent) + vowelCount(sent)) + punctuationCount(sent) + wordCount(sent) - 1 + " characters");
+        System.out.println(vowelCount(sent) + " vowels");
+        System.out.println(consonantCount(sent) + " consonants");
+        System.out.println(punctuationCount(sent) + " punctuation characters");
+
+        System.out.println();
     }
     public static boolean isVowel(char character) {
         boolean vowelChk;
@@ -19,8 +27,8 @@ public class A9_SentenceInformation {
     }
     public static boolean isConsonant(char character) {
         boolean consChk;
-        String vowels = "aeiouAEIOU";
-        if (!vowels.contains("" + character)) {
+        String cons = "qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM";
+        if (cons.contains("" + character)) {
             consChk = true;
         }
         else consChk = false;
@@ -28,18 +36,21 @@ public class A9_SentenceInformation {
     }
     public static boolean isPunctuation(char character) {
         boolean puncCheck;
-        String puncs = "!,.?;:";
-        if (!puncs.contains("" + character)) {
+        String puncs = "\'!,.?;:\"";
+        if (puncs.contains("" + character)) {
             puncCheck = true;
         }
         else puncCheck = false;
         return puncCheck;
     }
+
+    // counts *******************************************************************************
+
     public static int consonantCount(String text) {
         int totalcons = 0;
-        String vowels = "aeiouAEIOU";
+        String cons = "qwrtypsdfghjklzxcvbnmQWRTYPSDFGHJKLZXCVBNM";
         for(int iter = 0; iter < text.length(); iter++){
-            if (!vowels.contains("" + text.charAt(iter))) {
+            if (cons.contains("" + text.charAt(iter))) {
                 totalcons++;
             }
         }
@@ -55,15 +66,22 @@ public class A9_SentenceInformation {
         }
         return totalvows;
     }
+    public static int punctuationCount(String text) {
+        // iter thru entire text and add to var each time one found as a match - silly me forgor its the same as vowel and cons count
+        String puncs = "\'!,.?;:\"";
+        int totalpuncs = 0;
+        for (int iter = 0; iter < text.length(); iter++) {
+            if (puncs.contains("" + text.charAt(iter))) {
+                totalpuncs++;
+            }
+        }
+        return totalpuncs;
+    }
     public static int wordCount(String text) {
         // split text by spaces and return array length
         int totalwords = 0;
-        String vowels = "aeiouAEIOU";
-        for(int iter = 0; iter < text.length(); iter++){
-            if (vowels.contains("" + text.charAt(iter))) {
-                totalwords++;
-            }
-        }
+        String[] text2 = text.split(" ");
+        totalwords = text2.length;
         return totalwords;
     }
 }
