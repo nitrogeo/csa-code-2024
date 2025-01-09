82b3cb3bd07e9a94e6d377bac15ccb4d4ef7f049
nitrogeo bauguss <froxgames@outlook.com>
2025-01-09 15:55:40 -0600

    @ school csa backup bauguss

--- com.csa/src/A9/Labs/A9_PigLatin.java
+++ com.csa/src/A9/Labs/A9_PigLatin.java
@@ -3,4 +3,5 @@ package A9.Labs;
 public class A9_PigLatin {
     // move 1st part of word until vowel THEN add ay to end of each words after the 1st letter BUT:
     // y is vowel if not first letter and is the first "vowel", get to it before another vowel
+    // weekend days dont count as late in csa
 }
