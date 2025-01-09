7029605c6941051d1d6065f084327296e66956d8
nitrogeo bauguss <froxgames@outlook.com>
2025-01-09 15:54:55 -0600

    @ school csa backup bauguss

--- com.csa/src/A9/Labs/A9_PigLatin.java
+++ com.csa/src/A9/Labs/A9_PigLatin.java
@@ -1,5 +1,6 @@
 package A9.Labs;
 
 public class A9_PigLatin {
-    // move 1st letter to end of words THEN add ay to end of each words after the 1st letter
+    // move 1st part of word until vowel THEN add ay to end of each words after the 1st letter BUT:
+    // y is vowel if not first letter and is the first "vowel", get to it before another vowel
 }
