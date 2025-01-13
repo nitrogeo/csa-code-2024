7c5340efaf24ce8927f478a1078d4aa798ab145f
nitrogeo <froxgames@outlook.com>
2025-01-13 15:55:37 -0600

    @ school csa backup

--- com.csa/src/A10/CG1/A10_CH1.java
+++ com.csa/src/A10/CG1/A10_CH1.java
@@ -1,5 +1,15 @@
 package A10.CG1;
 
 public class A10_CH1 {
-    
+    public static double largest(double[][] data) {
+        double biggestbird = 0;
+        for (double[] datum : data) { // finds row
+            for (int iter1 = 0; iter1 < data[0].length; iter1++) { // finds column
+                if (datum[iter1] > biggestbird) {
+                    biggestbird = datum[iter1];
+                }
+            }
+        }
+        return biggestbird;
+    }
 }
