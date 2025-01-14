77cdbe32962f4f57fe3a47544ad2c0d92d64c658
nitrogeo-tully <froxgames@outlook.com>
2025-01-14 12:01:13 -0600

    @ school csa backup

--- com.csa/src/A9/CG1/A9_CH1.java
+++ com.csa/src/A9/CG1/A9_CH1.java
@@ -2,7 +2,7 @@ package A9.CG1;
 
 public class A9_CH1 {
     public static double largest(double[][] data) {
-        double biggestbird = 0;
+        double biggestbird = -100;
         for (int iter = 0; iter < data.length; iter++) { // finds row
             for (int iter1 = 0; iter1 < data[0].length; iter1++) { // finds column
                 if (data[iter][iter1] > biggestbird) {
