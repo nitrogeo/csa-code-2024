159b9e5f85536e9b43b13acbdb383d1e789a682f
nitrogeo <FroxGames@outlook.com>
2025-01-21 15:36:54 -0600

    @ home csa backup

--- com.csa/src/A10/CG2/A10_CH6.java
+++ com.csa/src/A10/CG2/A10_CH6.java
@@ -3,35 +3,64 @@ package A10.CG2;
 1. iter thru all 8 possible directions and see if theres one there.
 2. if so, add to kingsum (like a king in chess) and add to kingiter
 3. avg kingsum by kingiter and make that the next index in kingarray
+
+make var that checks if set to 0; if true, dont avg it
 */
 
 public class A10_CH6 {
-    public static boolean averageCells(int[][] data) {
-        int kingsum = 0;
-        int kingiter = 0;
-        int[][] kingarray;
+    public static int[] averageCells(int[][] data) {
+        int kingsum = 0; // total of that column to avg
+        int kingiter = 0; // number of rows added together (in order to avg them)
+        int[] kingarray = new int[data[0][0]];
+
+        boolean rplus0;
+        int rmin;
+        boolean rmin0;
+        //
+        boolean cplus0;
+        int cmin;
+        boolean cmin0;
+
+        int dul;
+        boolean dul0;
+        //
+        int dur;
+        boolean dur0;
+        //
+        int ddl;
+        boolean ddl0;
+        //
+        int ddr;
+        boolean ddr0;
 
         // pick an element...
         // r and c is current iter to check 8 adjacent ones of
-        for (int r = 0; r < data.length; r++) { // finds row
-            for (int c = 0; c < data[0].length; c++) { // finds column
+        for (int c = 0; c < data[0].length; c++) { // finds column
+            for (int r = 0; r < data.length; r++) { // finds row
                 // ************************* HORIZONTAL *****************************
-                // this saves the adjacent row values
+                // this saves the adjacent ROW values
                 int rplus =  data[r + 1][c];
-                int rmin;
+                // boolean rplus0;
+                // int rmin;
+                // boolean rmin0;
                 if (r != 0) { // if the row isnt 0 then set cmin to r - 1
                     rmin = data[r - 1][c];
                 }
                 else { // else set rmin to 0
                     rmin = data[r][c];
+                    rmin0 = true;
                 }
-                // this saves the adjacent column values
+                // this saves the adjacent COLUMN values
                 int cplus =  data[r][c + 1];
+                // boolean cplus0;
+                // int cmin;
+                // boolean cmin0;
                 if (c != 0) { // if the column isnt 0 then set cmin to c - 1
-                    int cmin = data[r][c - 1];
+                    cmin = data[r][c - 1];
                 }
                 else { // else set cmin to 0
-                    int cmin = data[r][c];
+                    cmin = data[r][c];
+                    cmin0 = true;
                 }
                 // that was horizontal,,, now check for diagonal...
 
@@ -43,40 +72,63 @@ public class A10_CH6 {
                 u = up; d = down
                 l = left, r = right
                 */
+                /*
+                int dul;
+                boolean dul0;
+                //
+                int dur;
+                boolean dur0;
+                //
+                int ddl;
+                boolean ddl0;
+                //
+                int ddr;
+                boolean ddr0;
+                */
+
                 // dul ******************************************************
                 if (c != 0) { // if the column isnt 0 then set cmin to c - 1
-                    int dul = data[r - 1][c - 1];
+                    dul = data[r - 1][c - 1];
                 }
                 else { // else set dplus to 0
-                    int dul = data[r][c];
+                    dul = data[r][c];
+                    dul0 = true;
                 }
                 // dur ******************************************************
                 if (c != 0) { // if the column isnt 0 then set cmin to c - 1
-                    int dur = data[r - 1][c + 1];
+                    dur = data[r - 1][c + 1];
                 }
                 else { // else set dplus to 0
-                    int dur = data[r][c];
+                    dur = data[r][c];
+                    dur0 = true;
                 }
                 // ddl ******************************************************
                 if (c != 0) { // if the column isnt 0 then set cmin to c - 1
-                    int ddl = data[r + 1][c - 1];
+                    ddl = data[r + 1][c - 1];
                 }
                 else { // else set dplus to 0
-                    int ddl = data[r][c];
+                    ddl = data[r][c];
+                    ddl0 = true;
                 }
                 // ddr ******************************************************
                 if (c != 0) { // if the column isnt 0 then set cmin to c - 1
-                    int ddr = data[r + 1][c + 1];
+                    ddr = data[r + 1][c + 1];
                 }
                 else { // else set dplus to 0
-                    int ddr = data[r][c];
+                    ddr = data[r][c];
+                    ddr0 = true;
                 }
                 // ************************* END SAVING VALUES *****************************
 
                 // now iter thru all elements and check if 1 "square" away from te current iter...
+                // ...
+                /* actually,,, nvm. iter thru all adjacent values saved from the variables above and
+                check if each one's 0 bool is true. if so, dont avg it. if its false, avg it and put the avg in
+                the current indx of int[][] kingarray.
+                 */
 
                 // r1 and c1 are directional check iters
-                for (int r1 = 0; r < data.length; r++) { // row to check
+                /* for (int r1 = 0; r < data.length; r++) { // row to check
                     for (int c1 = 0; c < data[0].length; c++) { // column to check
                         if (data[r1] == data[r]) { // if in same row, ...
                             // ...then check if ((column indx +1 or -1) = current element indx)...
@@ -88,9 +140,45 @@ public class A10_CH6 {
                             break;
                         }
                     }
+                }*/
+                {
+                    if (rplus0 = false) {
+                        kingsum += rplus;
+                        kingiter++;
+                    }
+                    if (rmin0 = false) {
+                        kingsum += rmin;
+                        kingiter++;
+                    }
+                    if (cplus0 = false) {
+                        kingsum += rmin;
+                        kingiter++;
+                    }
+                    if (cmin0 = false) {
+                        kingsum += rmin;
+                        kingiter++;
+                    }
+                    //
+                    if (dul0 = false) {
+                        kingsum += rmin;
+                        kingiter++;
+                    }
+                    if (dur0 = false) {
+                        kingsum += rmin;
+                        kingiter++;
+                    }
+                    if (ddl0 = false) {
+                        kingsum += rmin;
+                        kingiter++;
+                    }
+                    if (ddr0 = false) {
+                        kingsum += rmin;
+                        kingiter++;
+                    }
                 }
+                kingarray[c] = kingsum / kingiter;
             }
         }
-        return same;
+        return kingarray;
     }
 }
