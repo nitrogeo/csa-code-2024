0de119948e8f21e908f8837d6ee84ddfcac65ad8
nitrogeo-tully <froxgames@outlook.com>
2025-02-13 12:46:54 -0600

    @ school csa backup

---
+++ com.csa/src/A10/CG1/A11_CH4.java
@@ -0,0 +1,21 @@
+package A10.CG1;
+
+import java.util.ArrayList;
+/*
+iter thru toremove
+iter thru text
+search for current target and terminate them
+*/
+public class A11_CH4 {
+    public static void removeInts(ArrayList<Integer> numbers, int[] toRemove) {
+        for (int iter = numbers.size() -1; iter >= 0; iter--) {
+
+
+            for (int iter1 = numbers.size() -1; 1 >= 0; iter1--) {
+                if (numbers.get() == toRemove[iter]) {
+                    numbers.remove(x)
+                }
+            }
+        }
+    }
+}
--- com.csa/src/A11/CG1/A11_CH3.java
+++ com.csa/src/A11/CG1/A11_CH3.java
@@ -2,15 +2,26 @@ package A11.CG1;
 
 import java.util.ArrayList;
 
+// make right ans and replace the indx with the right ans
+/*
+iter thru array
+make current index crashtest
+iter thru variable and if iter is capital add it to crashsave
+*/
+
 public class A11_CH3 {
-    String x = "";
-
-    public static void main(String[] args) {
-
-    }
     public static void capitals(ArrayList<String> text) {
-        for (int y = 0; y < text.size(); y++) {
-            if capitals(u);
+        String crashtest = "";
+        String crashsave = "";
+        String caps = "QWERTYUIOPASDFGHJKLZXCVBNM";
+        for (int iter = 0; iter < text.size(); iter++) {
+            crashtest = text.get(iter);
+
+            for (int iter1 = 0; iter1 < crashtest.length(); iter1++) {
+                if (caps.contains("" + crashtest.charAt(iter1))) {
+                    crashsave+=crashsave.charAt(iter1);
+                }
+            }
         }
     }
 }
