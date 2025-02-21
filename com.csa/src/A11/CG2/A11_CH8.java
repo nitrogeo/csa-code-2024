8f3c348d4a24e6aeebdb9dc9d6a4b501377a1b7a
nitrogeo-tully <froxgames@outlook.com>
2025-02-21 12:44:57 -0600

    @ school csa backup

--- com.csa/src/A11/CG2/A11_CH8.java
+++ com.csa/src/A11/CG2/A11_CH8.java
@@ -1,14 +1,26 @@
 package A11.CG2;
 
 import java.util.ArrayList;
+import java.awt.*;
 
 public class A11_CH8 {
     /*
+        iter thru data and check if x is neg and y is neg
+        if so, passers++
 
+        gets x and y of current iter bc current iter has both
     */
-    public static int x;
-    public static int y;
-    public static int quadrantCount(ArrayList<Integer> data) {
-        asd
+
+    // done 12:43 2.21.2025
+
+
+    public static int quadrantCount(ArrayList<Point> data) {
+        int passers = 0;
+        for (int iter = 0; iter < data.size(); iter++) {
+            if ((data.get(iter).getX() < 0) && data.get(iter).getY() < 0) {
+                passers++;
+            }
+        }
+        return passers;
     }
 }
