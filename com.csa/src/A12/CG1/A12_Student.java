2ad2254afb69aae2840ea8413674510d441a08a5
nitrogeo <FroxGames@outlook.com>
2025-03-17 22:40:00 -0500

    @ home csa backup

--- com.csa/src/A12/CG1/A12_Student.java
+++ com.csa/src/A12/CG1/A12_Student.java
@@ -1,23 +1,37 @@
 package A12.CG1;
 
+// done 10:38pm 3.17
+
 public class A12_Student extends A12.CG1.A12_Person {
     // attributes
     public static int id;
 
     // constructors
     
-    A12_Student(String firstName, String lastName, int id) {
+    public A12_Student(String firstName, String lastName, int id) {
+        super(firstName, lastName);
 
+        this.id = id;
     }
 
     // methods
-    int getID() {
-
+    public int getID() {
+        return id;
     }
-    boolean equals(Object o) {
 
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof A12_Student) {
+            // cast to a12
+            A12_Student student = (A12_Student) o;
+
+            return super.equals(o) && student.getFirstName().equals(((A12_Student) o).getFirstName()) && student.getLastName().equals(((A12_Student) o).getLastName());
+        }
+        else {
+            return false;
+        }
     }
-    String toString() {
-
+    public String toString() {
+        return (id + " - " + getLastName() + ", " + getFirstName());
     }
 }
