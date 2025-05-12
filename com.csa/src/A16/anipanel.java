3f31ca16ef5f19a4cdb8fdddfe0837b3a4c246df
nitrogeo-tully <froxgames@outlook.com>
2025-05-12 12:47:56 -0500

    @ school csa backup

--- com.csa/src/A16/anipanel.java
+++ com.csa/src/A16/anipanel.java
@@ -10,7 +10,7 @@ public class anipanel extends JPanel {
         super();
 
         // sets the size of the panel
-        setSize(300, 300);
+        setSize(1000, 800);
     }
 
     public void paint (Graphics g) {
@@ -21,7 +21,30 @@ public class anipanel extends JPanel {
         g.drawLine (0, 0, getWidth(), getHeight());
         g.drawLine(getWidth(), 0, 0, getHeight());*/
 
-        g.setColor(new Color(240, 220, 133));
+        // sky = baby blue
+        g.setColor(new Color(102, 146, 198)); // or 133 for blue idk
         g.fillRect(0, 0, getWidth(), getHeight());
+
+        // clouds = greyish pink
+        g.setColor(new Color(236, 216, 216)); // or 133 for blue idk
+
+        int[] xPoints1 = {780, 653, 670, 795, 795, 548, 828, 828, 1000, 1000, 780};
+        int[] yPoints1 = {0, 77, 118, 88, 118, 216, 180, 265, 216, 0, 0};
+
+        g.fillPolygon(xPoints1, yPoints1, xPoints1.length);
+
+        int[] xPoints1 = {780, 653, 670, 795, 795, 548, 828, 828, 1000, 1000, 780};
+        int[] yPoints1 = {0, 77, 118, 88, 118, 216, 180, 265, 216, 0, 0};
+
+        g.fillPolygon(xPoints1, yPoints1, xPoints1.length);
+
+
+        // surface ground = beige
+        g.setColor(new Color(204, 176, 92 )); // or 133 for blue idk
+        g.fillRect(0, 600, getWidth(), getHeight());
+
+        // below ground = dark brown
+        g.setColor(new Color(132, 58, 22));
+        g.fillRect(0, 620, getWidth(), getHeight());
     }
 }
